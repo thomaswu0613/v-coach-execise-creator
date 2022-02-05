4e74a02d36d2d94791c65d0d3fe8ec089a30dd58
Thomas Wu <wutimmy01@gmail.com>
2022-02-05 22:08:07 +0800

    finished whole app, may need improvement in the furture

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+from email import message
+import traceback
+from turtle import title
 import PySimpleGUI as sg
 import cv2
 from time import sleep
@@ -5,18 +8,23 @@ import numpy as np
 from tomlkit import value
 from tools import detect_pose_with_draw, BodyLandMarks
 import mediapipe as mp
-layout = [[sg.Text('OpenCV Demo', size=(40, 1), justification='center', font='Helvetica 20',)],
-            [sg.Image(filename='', key='image'),sg.Listbox('',size=(30,30),key='listbox1')],
+import json
+from pathlib import Path
+layout = [[sg.Text('V-coach Execise Creator', size=(40, 1), justification='center', font='Helvetica 20',),sg.Text('Frame Count : ', size=(40, 1), justification='center', font='Helvetica 20',key="frame_count"),],
+            [sg.Image(filename='', key='image'),sg.Listbox('',size=(30,30),key='listbox1',enable_events=True)],
             [sg.Image(filename='', key='image1')],
             [sg.Text('', size=(40, 1), justification='center', font='Helvetica 20',key="txt1"),],
-            [sg.FileBrowse('Open Video File',file_types=(("Video Files", "*.mp4",),("Video Files","*.avi"),("Video Files","*.MOV"),)),sg.Button('Load Video', size=(10, 1), font='Helvetica 14'),
-            sg.Button('Create Stage', size=(10, 1), font='Any 14',key='create_stage'),
-            sg.Button('Play Again', size=(10, 1), font='Any 14'),
-            sg.Button('Exit', size=(10, 1), font='Helvetica 14'), ]]
+            [sg.Text('Video Controls :', size=(40, 1), justification='center',font='Helvetica 20',border_width=0),sg.FileBrowse('Open Video File',file_types=(("Video Files", "*.mp4",),("Video Files","*.avi"),("Video Files","*.MOV"),),key="open"),sg.Button('Load Video', size=(10, 1), font='Helvetica 14'),
+            sg.Button('Play Again', size=(10, 1), font='Any 14')],
+            [sg.Text('Stage Edit :', size=(40, 1), justification='center'),sg.Button('Create Stage', size=(10, 1), font='Any 14',key='create_stage'),sg.Button('Finish and Write Stages', size=(10, 1), font='Any 14',key='finish'),sg.Button('Exit', size=(10, 1), font='Helvetica 14'), ]]
+
+enter_name_win_layout = [[sg.Text('Please Enter the name of execise you created:', size=(40, 1), justification='center', font='Helvetica 20',),sg.InputText(key="exe_name")],
+                        [sg.Button('Submit', size=(10, 1), font='Any 14',key='submit'),sg.Button('Exit', size=(10, 1), font='Any 14',key='exit')]]
 
 
 window = sg.Window('Demo Application - OpenCV Integration',
                     layout, location=(800, 400),return_keyboard_events=True)
+
 
 
 video = False
@@ -24,34 +32,28 @@ pause = False
 # reload = False
 
 listboxone = []
-stages_to_write = {}
+stages_to_write = []
 mp_pose = mp.solutions.pose
 frame_count = 0
 stage_count = 0
 
 while True:
     event, values = window.read(timeout=10)
-    # print(event)
+    print(event)
     if event == 'Exit' or event == sg.WIN_CLOSED or event == "Escape:9":
-        for k,v in stages_to_write.items():
-            print(k,v)
         window.close()
         cap.release()
         break
+    
+    if event == "open":
+        window["txt1"].update(value="Press 'Load Video' button to loa the video.")
+
     if event == 'Load Video':
-        video_path = values["Open Video File"]
+        video_path = values["open"]
         video = True
         pause = True
         cap = cv2.VideoCapture(video_path)
         # cap = cv2.VideoCapture("/home/thomasw/workspace/execise-creator-app/IMG_105610890.MOV")
-        loading_screen_layout = [[sg.Text('Loading..', size=(40, 1), justification='center', font='Helvetica 30')],
-                        [sg.ProgressBar(
-                            max_value=30,
-                            orientation='h',
-                            size=(20, 20),
-                            border_width=4,
-                            key='progbar',
-                            bar_color=['Red','Green'])]]
 
     if event == 'Play Again':
         cap = cv2.VideoCapture(video_path)
@@ -64,12 +66,48 @@ while True:
 
     if event == 'create_stage':
         lm = BodyLandMarks(results, mp_pose)
-        stages_to_write[stage_count] = [lm.return_all_points(),frame_count]
+        stages_to_write.append(lm.return_all_points())
         print("Created stage {}".format(int(stage_count)))
         listboxone.append("Stage {}: Frame No. {}".format(str(stage_count),str(frame_count)))
         window['listbox1'].update(listboxone)
         stage_count+=1
         print(stages_to_write)
+
+    if event == "finish":
+        name_win = sg.Window("Enter Execise Name",enter_name_win_layout)
+        while True:
+            e,v = name_win.read()
+            if e == "exit":
+                name_win.close()
+                break
+            if e == "submit":
+                wpath = sg.popup_get_folder(message="Please select the location to save the execise file.")
+                try:
+                    Path("{}/{}".format(wpath,v["exe_name"])).mkdir(parents=True, exist_ok=True)
+                except Exception as e:
+                    tb=traceback.format_exc()
+                    sg.popup_error(f'AN EXCEPTION OCCURRED!', e, tb)
+                else:
+                    stage_counter = 0
+                    s = False
+                    for i in range(len(stages_to_write)):
+                        try:
+                            with open("{}/{}/stage{}.json".format(wpath,v["exe_name"],str(i)),"w+",encoding="utf8") as f:
+                                json.dump(stages_to_write[i],f)
+                                f.close()
+                        except Exception as e:
+                            s = False
+                            tb=traceback.format_exc()
+                            sg.popup_error(f'AN EXCEPTION OCCURRED!', e, tb)
+                        else:
+                            s = True
+                    name_win.close()
+                    if s:
+                        sg.popup_ok(title="Create Sucessful !")
+                    else:
+                        sg.popup_ok(title="Create Failed !")
+                    break
+
         
 
     if video:
@@ -90,7 +128,9 @@ while True:
         frame, results = detect_pose_with_draw(resized)
         window["image1"].update(data=cv2.imencode('.png', frame)[1].tobytes())
         frame_count+=1
+        window["frame_count"].update(value="Frame Count : {}".format(str(frame_count)))
     else:
         window["txt1"].update(value="")
+        frame_count = 0
 
 
