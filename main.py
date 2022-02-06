15a0e8d94c21f86c875cf7a1f605a78bea6c95cd
Thomas Wu <wutimmy01@gmail.com>
2022-02-06 08:52:38 +0800

    redesigned submit dialog

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
-from email import message
+import os
 import traceback
-from turtle import title
+from pathlib import Path
 import PySimpleGUI as sg
 import cv2
-from time import sleep
 import numpy as np
-from tomlkit import value
 from tools import detect_pose_with_draw, BodyLandMarks
 import mediapipe as mp
-import json
-from pathlib import Path
 import yaml
+
+def submit_layout():
+    return [[sg.Text('Please Enter the name of execise you created:', size=(60, 1), justification='center', font='Helvetica 20',key="txt3"),sg.InputText(key="exe_name")],
+                        [sg.Text('Please Select the path where you want to save the files:', size=(60, 1), justification='center', font='Helvetica 20',key="txt4"),sg.InputText(key="exe_name"),sg.FolderBrowse('Select Folder',key='select_folder')],
+                        [sg.Button('Submit', size=(10, 1), font='Any 14',key='submit'),sg.Button('Quit Program', size=(15, 1), font='Any 14',key="quit"),sg.Button('Close this dialog and continue', size=(30, 1), font='Any 14',key="exit")]]
+
+
 layout = [[sg.Text('V-coach Execise Creator', size=(40, 1), justification='center', font='Helvetica 20',),sg.Text('Frame Count : ', size=(40, 1), justification='center', font='Helvetica 20',key="frame_count"),],
             [sg.Image(filename='', key='image'),sg.Listbox('',size=(30,30),key='listbox1',enable_events=True)],
             [sg.Image(filename='', key='image1')],
             [sg.Text('', size=(40, 1), justification='center', font='Helvetica 20',key="txt1"),],
-            [sg.Text('Video Controls :', size=(40, 1), justification='center',font='Helvetica 20',border_width=0),sg.FileBrowse('Open Video File',file_types=(("Video Files", "*.mp4",),("Video Files","*.avi"),("Video Files","*.MOV"),),key="open"),sg.Button('Load Video', size=(10, 1), font='Helvetica 14'),
+            [sg.Text('Video Controls :', size=(60, 1), justification='center',font='Helvetica 20',border_width=0),sg.FileBrowse('Open Video File',file_types=(("Video Files", "*.mp4",),("Video Files","*.avi"),("Video Files","*.MOV"),),key="open"),sg.Button('Load Video', size=(10, 1), font='Helvetica 14'),
             sg.Button('Play Again', size=(10, 1), font='Any 14')],
-            [sg.Text('Stage Edit :', size=(40, 1), justification='center'),sg.Button('Create Stage', size=(10, 1), font='Any 14',key='create_stage'),sg.Button('Finish and Write Stages', size=(10, 1), font='Any 14',key='finish'),sg.Button('Exit', size=(10, 1), font='Helvetica 14'), ]]
-
-enter_name_win_layout = [[sg.Text('Please Enter the name of execise you created:', size=(40, 1), justification='center', font='Helvetica 20',),sg.InputText(key="exe_name")],
-                        [sg.Button('Submit', size=(10, 1), font='Any 14',key='submit'),sg.Button('Exit', size=(10, 1), font='Any 14',key='exit')]]
-
+            [sg.Text('Stage Edit :', size=(60, 1), justification='center'),sg.Button('Create Stage', size=(15, 1), font='Any 14',key='create_stage',visible=False),sg.Button('Finish and Write Stages', size=(25, 1), font='Any 14',key='finish'),sg.Button('Exit', size=(10, 1), font='Helvetica 14'), ]]
 
 window = sg.Window('Demo Application - OpenCV Integration',
                     layout, location=(800, 400),return_keyboard_events=True)
@@ -36,7 +35,7 @@ listboxone = []
 stages_to_write = []
 mp_pose = mp.solutions.pose
 frame_count = 0
-stage_count = 0
+stage_count = 1
 
 while True:
     event, values = window.read(timeout=10)
@@ -62,7 +61,6 @@ while True:
 
     if event == 'space:65':
         pause = bool(abs(1-int(pause)))
-        sleep(1)
 
     if event == 'create_stage':
         lm = BodyLandMarks(results, mp_pose)
@@ -74,46 +72,40 @@ while True:
         print(stages_to_write)
 
     if event == "finish":
-        name_win = sg.Window("Enter Execise Name",enter_name_win_layout)
+        submit_window = sg.Window("Enter Execise Name",submit_layout())
+
         while True:
-            e,v = name_win.read()
+            e,v = submit_window.read(timeout=20)
+            print(e,v)
             if e == "exit":
-                name_win.close()
+                submit_window.close()
+                break
+            if e == "quit":
+                submit_window.close()
+                window.close()
                 break
             if e == "submit":
-                wpath = sg.popup_get_folder(message="Please select the location to save the execise file.")
                 try:
-                    Path("{}/{}".format(wpath,v["exe_name"])).mkdir(parents=True, exist_ok=True)
+                    path_to_create = "{}/{}".format(v["select_folder"],v["exe_name"])
+                    os.makedirs(path_to_create, exist_ok=False)
+                    with open("{}/stages.yaml".format(path_to_create),"w+") as f:
+                        for i in range(len(stages_to_write)):
+                            f.seek(0,2)
+                            print("writing things")
+                            yaml.dump({'stage{}'.format(i+1):stages_to_write[i]},f)
+                except FileExistsError:
+                    sg.popup_error("Folder Exists! Please rename your execise.")
                 except Exception as e:
                     tb=traceback.format_exc()
-                    sg.popup_error(f'AN EXCEPTION OCCURRED!', e, tb)
+                    sg.popup_error("Error!",e,tb)
                 else:
-                    stage_counter = 0
-                    s = False
-                    for i in range(len(stages_to_write)):
-                        try:
-                            with open("{}/{}/stage{}.json".format(wpath,v["exe_name"],str(i)),"w+",encoding="utf8") as f:
-                                json.dump(stages_to_write[i],f)
-                                f.close()
-                        except Exception as e:
-                            s = False
-                            tb=traceback.format_exc()
-                            sg.popup_error(f'AN EXCEPTION OCCURRED!', e, tb)
-                        else:
-                            s = True
-                    name_win.close()
-                    with open("{}/{}/config.yaml".format(wpath,v["exe_name"]),"w+") as f:
-                        yaml.dump({"stages_count":len(stages_to_write)})
-                        f.close()
-                    if s:
-                        sg.popup_ok(title="Create Sucessful !")
-                    else:
-                        sg.popup_ok(title="Create Failed !")
+                    sg.popup_ok("Creation Success!")
+                    submit_window.close()
                     break
-
         
 
     if video:
+        window["create_stage"].update(visible=True)
         window["txt1"].update(value="Press space bar to play the video.")
         if pause:
             continue
@@ -134,6 +126,6 @@ while True:
         window["frame_count"].update(value="Frame Count : {}".format(str(frame_count)))
     else:
         window["txt1"].update(value="")
+        window["create_stage"].update(visible=False)
         frame_count = 0
-
 
