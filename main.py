3dc6429a5fc37bfb32509d6e59231f9c02f55488
Thomas Wu <wutimmy01@gmail.com>
2022-02-07 11:51:24 +0800

    add support for scoring-needed file creation

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from tools import detect_pose_with_draw, BodyLandMarks
 import mediapipe as mp
 import yaml
 from shutil import copyfile
+import subprocess, shlex
 
 def submit_layout():
     return [[sg.Text('Please Enter the name of execise you created:', size=(60, 1), justification='center', font='Helvetica 20',key="txt3"),sg.InputText(key="exe_name")],
@@ -23,13 +24,15 @@ layout = [[sg.Text('V-coach Execise Creator', size=(40, 1), justification='cente
             sg.Button('Play Again', size=(10, 1), font='Any 14')],
             [sg.Text('Stage Edit :', size=(60, 1), justification='center'),sg.Button('Create Stage', size=(15, 1), font='Any 14',key='create_stage',visible=False),sg.Button('Finish and Write Stages', size=(25, 1), font='Any 14',key='finish'),sg.Button('Exit', size=(10, 1), font='Helvetica 14'), ]]
 
-window = sg.Window('Demo Application - OpenCV Integration',
+window = sg.Window('V-coach Execise Creator',
                     layout, location=(800, 400),return_keyboard_events=True)
 
 
 
 video = False
 pause = False
+wait = False
+processing = False
 # reload = False
 
 listboxone = []
@@ -37,6 +40,8 @@ stages_to_write = []
 mp_pose = mp.solutions.pose
 frame_count = 0
 stage_count = 1
+scoring_lib_dir = "/home/thomasw/workspace/v-coach-pose-training-app/scoring_lib"
+python_path = "/home/thomasw/anaconda3/bin/python"
 
 while True:
     event, values = window.read(timeout=10)
@@ -66,18 +71,15 @@ while True:
     if event == 'create_stage':
         lm = BodyLandMarks(results, mp_pose)
         stages_to_write.append(lm.return_all_points())
-        print("Created stage {}".format(int(stage_count)))
         listboxone.append("Stage {}: Frame No. {}".format(str(stage_count),str(frame_count)))
         window['listbox1'].update(listboxone)
         stage_count+=1
-        print(stages_to_write)
 
     if event == "finish":
         submit_window = sg.Window("Enter Execise Name",submit_layout())
 
         while True:
             e,v = submit_window.read(timeout=20)
-            print(e,v)
             if e == "exit":
                 submit_window.close()
                 break
@@ -85,18 +87,35 @@ while True:
                 submit_window.close()
                 window.close()
                 break
-            if e == "submit":
+            if e == "submit" or wait is True:
                 try:
-                    path_to_create = "{}/{}".format(v["select_folder"],v["exe_name"])
-                    os.makedirs(path_to_create, exist_ok=False)
-                    nom_path = os.path.normpath(video_path)
-                    path_com = nom_path.split(os.sep)
-                    copyfile(video_path,str(path_to_create+"/{}".format(path_com[-1])))
-                    with open("{}/stages.yaml".format(path_to_create),"w+") as f:
-                        for i in range(len(stages_to_write)):
-                            yaml.dump({'stage{}'.format(i+1):stages_to_write[i]},f)
-                    with open("{}/config.yaml".format(path_to_create),"w+") as f:
-                        yaml.dump({'max_stages':len(stages_to_write)})
+                    if wait is False:
+                        path_to_create = "{}/{}".format(v["select_folder"],v["exe_name"])
+                        os.makedirs(path_to_create, exist_ok=False)
+                        nom_path = os.path.normpath(video_path)
+                        path_com = nom_path.split(os.sep)
+                        copyfile(video_path,str(path_to_create+"/{}".format(path_com[-1])))
+                        with open("{}/stages.yaml".format(path_to_create),"w+") as f:
+                            for i in range(len(stages_to_write)):
+                                yaml.dump({'stage{}'.format(i+1):stages_to_write[i]},f)
+                        with open("{}/config.yaml".format(path_to_create),"w+") as f:
+                            yaml.dump({'max_stages':len(stages_to_write)})
+                    wait = True
+                    args = shlex.split("{}/keypoints_from_video.py --activity {} --video {} --lookup {}/lookup_{}.pickle".format(scoring_lib_dir,v["exe_name"],video_path,path_to_create,v["exe_name"]))
+                    if processing is False:
+                        p=subprocess.Popen(args,stdout=subprocess.PIPE)
+                        processing = True
+                    out, err = p.communicate()
+                    if p.poll() is None:
+                        wait = True
+                        processing = True
+                        continue
+                    else: 
+                        if "Lookup Table Created" in out.decode():
+                            sg.popup_ok("Creation Success!")
+                            break
+                        else:
+                            sg.popup_error("Create Failed!")
                 except FileExistsError:
                     sg.popup_error("Folder Exists! Please rename your execise.")
                 except Exception as e:
@@ -104,7 +123,7 @@ while True:
                     sg.popup_error("Error!",e,tb)
                 else:
                     sg.popup_ok("Creation Success!")
-                    submit_window.close()
+
                     break
         
 
